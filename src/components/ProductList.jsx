1cf58ba8e209f7e147a496787af2f001be989123
Assam157 <asasm8106@gmail.com>
2024-11-20 19:51:30 +0530

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -16,7 +16,7 @@ const ProductList = () => {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await axios.get('http://localhost:3001/api/products'); // Adjust the API endpoint as needed
+                const response = await axios.get('https://backendju-3.onrender.com//api/products'); // Adjust the API endpoint as needed
                 const products = response.data;
                 console.log("Are Here",products)
                 // Extract unique product types from the products
