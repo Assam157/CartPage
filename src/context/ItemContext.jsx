5e3ef5826b12ebd06191f8244ed0cc5d46ecfcb1
Assam157 <asasm8106@gmail.com>
2024-11-22 10:36:45 +0530

    Update ItemContext.jsx

--- src/context/ItemContext.jsx
+++ src/context/ItemContext.jsx
@@ -12,7 +12,7 @@ export const ItemProvider = ({ children }) => {
     
     const fetchProducts = async () => {
   try {
-    const response = await fetch('https://backendju-3.onrender.com/api/products', {
+    const response = await fetch('https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/api/products', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
