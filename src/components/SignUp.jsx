a33eae893689a708a2bcd3268e195728bdfccaa7
Assam157 <asasm8106@gmail.com>
2025-03-13 15:49:57 +0530

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -12,7 +12,7 @@ const SignUp = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch("https://shoppingpage-in.com/signup", {
+            const response = await fetch("https://shoppingpage.in/signup", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
