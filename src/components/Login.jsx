c825e62d5dcca102511d040448669c9b35ddbb98
Assam157 <asasm8106@gmail.com>
2025-03-13 15:49:19 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch("https://shoppingpage-in.com/login", {
+            const response = await fetch("https://shoppingpage.in/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
