ff1dfd2f5427dc185781af8177b0caa3db97edc0
Assam157 <asasm8106@gmail.com>
2025-03-13 15:50:21 +0530

    Update ItemContext.jsx

--- src/context/ItemContext.jsx
+++ src/context/ItemContext.jsx
@@ -12,7 +12,7 @@ export const ItemProvider = ({ children }) => {
     
     const fetchProducts = async () => {
   try {
-    const response = await fetch('https://shoppingpage-in.com/api/products', {
+    const response = await fetch('https://shoppingpage.in/api/products', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
